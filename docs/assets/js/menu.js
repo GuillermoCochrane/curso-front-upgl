6f0707c370ba35d9a7eba8eef691379960affc09
GuillermoCochrane <guilleac81@gmail.com>
2025-07-14 02:25:49 -0300

    Script modificado, para mostrar menu nav-bar del header, para resolciones menores a 1200px

--- docs/assets/js/menu.js
+++ docs/assets/js/menu.js
@@ -2,6 +2,7 @@
 window.addEventListener("load" , () => {
   const toggleBtn = document.querySelector("#menu-toggle");
   const sidebar = document.querySelector("#sidebar");
+  const menu = document.querySelector("#menu");
   const menuOpenIcon = document.querySelector("#menu-open-icon");
   const menuCloseIcon = document.querySelector("#menu-close-icon");
 
@@ -9,5 +10,8 @@ window.addEventListener("load" , () => {
     sidebar?.classList.toggle("hidden");
     menuOpenIcon?.classList.toggle("unseen");
     menuCloseIcon?.classList.toggle("unseen");
+    if (window.innerWidth < 1200) {      // si el viewport es menor a 1200px
+      menu?.classList.toggle("hidden");
+    }
   });
 });
