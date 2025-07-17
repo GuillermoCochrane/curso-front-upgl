ec93dd2af100e5d1ab8ab54371966087e59d7e35
GuillermoCochrane <guilleac81@gmail.com>
2025-07-17 17:57:35 -0300

    Script y estilos modificado para indexbar en mobile

--- docs/assets/js/menu.js
+++ docs/assets/js/menu.js
@@ -2,16 +2,21 @@
 window.addEventListener("load" , () => {
   const toggleBtn = document.querySelector("#menu-toggle");
   const sidebar = document.querySelector("#sidebar");
+  const indexbar = document.querySelector("#indexbar");
   const menu = document.querySelector("#menu");
   const menuOpenIcon = document.querySelector("#menu-open-icon");
   const menuCloseIcon = document.querySelector("#menu-close-icon");
 
   toggleBtn?.addEventListener("click", () => {
-    sidebar?.classList.toggle("hidden");
+
     menuOpenIcon?.classList.toggle("unseen");
     menuCloseIcon?.classList.toggle("unseen");
     if (window.innerWidth < 1200) {      // si el viewport es menor a 1200px
       menu?.classList.toggle("hidden");
+      indexbar?.classList.toggle("hidden");
+    }
+    if (window.innerWidth >= 1200) {      // si el viewport es mayor a 1200px
+    sidebar?.classList.toggle("hidden");
     }
   });
 });
