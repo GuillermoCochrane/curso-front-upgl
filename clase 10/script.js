0261070d9fa97433c9bda7209d3f1a1d299e4462
GuillermoCochrane <guilleac81@gmail.com>
2025-08-07 15:55:13 -0300

    Ejemplo hecho en la clase presencial 10

---
+++ clase 10/script.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function() {
+/*     console.log("Hola Mundo")
+    document.write("<h1>Hola Mundo</h1>")
+    document.write("<p>Esto es un parrafo dinamico</p>") */
+})
