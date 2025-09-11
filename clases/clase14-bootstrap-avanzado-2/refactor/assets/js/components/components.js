661cc65546bb0f8e3ae56939105e6381b177e02e
GuillermoCochrane <guilleac81@gmail.com>
2025-09-11 04:43:10 -0300

    Agregadas función para la creación del componente del producto

--- clases/clase14-bootstrap-avanzado-2/refactor/assets/js/components/components.js
+++ clases/clase14-bootstrap-avanzado-2/refactor/assets/js/components/components.js
@@ -1,14 +1,24 @@
 import { $, $$, createElement, createImage, createButton } from '../utilities/dom.js';
+import { filterProduct } from '../utilities/utilities.js';
 
 // Crea componente de datos del producto
 export function createCardInfo(nombre, descripcion, id) {
     const $cardInfo = createElement('div', 'card-body');
-    const $title = createElement('h5', 'card-title');
-    const $text = createElement('p', 'card-text');
+    const $title = createElement('h5', 'card-title product-card-title');
+    const $text = createElement('p', 'card-text product-card-text');
     const $button = createButton('Ver Detalles', 'btn btn-dark', 'btn-ver-detalles', 'modal', 'detallesModal');
     $title.textContent = nombre ? nombre : 'Zapatilla Generica';
     $text.textContent = descripcion ? descripcion : 'Descripción de la zapatilla';
     $button.setAttribute('data-zapatilla', id ? id : 'zapatilla-generica');
     $cardInfo.append($title, $text, $button);
     return $cardInfo;
+}
+
+// Crea componente de producto
+export function createProductCard(product) {
+    const $productCard = createElement('article', 'card product-card');
+    const $image = createImage(product.url ? product.url : './assets/img/default.png', product.nombre ? product.nombre : 'Zapatilla Generica', 'card-img-top');
+    const $cardInfo = createCardInfo(product.nombre, product.descripcion, product.id);
+    $productCard.append($image, $cardInfo);
+    return $productCard;
 }
