2c7049ff54b86d38b18907806fec465288c05962
GuillermoCochrane <guilleac81@gmail.com>
2025-09-11 01:49:16 -0300

    DB actualizada

--- clases/clase14-bootstrap-avanzado-2/script.js
+++ clases/clase14-bootstrap-avanzado-2/script.js
@@ -5,7 +5,7 @@ const zapatillas = {
     descripcion: "El icono del basquetbol de los 80, que se creó para la cancha pero conquistó las calles del mundo.",
     talles: "36, 37, 38, 43, 45",
     colores: "Blanco, Rojo, Negro",
-    url: "https://nikearprod.vtexassets.com/arquivos/ids/378649-800-800?width=800&height=800&aspect=true"
+    url: "https://nikearprod.vtexassets.com/arquivos/ids/378649-800-800"
     },
     "air-jordan-1-low": {
       nombre: "Air Jordan 1 Low",
@@ -13,7 +13,7 @@ const zapatillas = {
       descripcion: "La unidad encapsulada Air-Sole proporciona amortiguación ligera y un diseño de alta calidad para mayor comodidad.",
       talles: "36, 37, 38, 43, 45",
       colores: "Blanco, Rojo, Negro",
-      url: "https://nikearprod.vtexassets.com/arquivos/ids/981162-800-800?width=800&height=800&aspect=true"
+      url: "https://nikearprod.vtexassets.com/arquivos/ids/981162-800-800"
     },
     "nike-pegasus-40": {
       nombre: "Nike Pegasus 40",
@@ -21,7 +21,7 @@ const zapatillas = {
       descripcion: "Con una pisada elástica para cada tipo de carrera, vuelve la sensación ideal para ayudarte a alcanzar tus objetivos",
       talles: "36, 37, 38, 43, 45",
       colores: "Blanco, Rojo, Negro",
-      url: "https://nikearprod.vtexassets.com/arquivos/ids/701565-800-800?width=800&height=800&aspect=true"
+      url: "https://nikearprod.vtexassets.com/arquivos/ids/701565-800-800"
     },
     "adidas-rivalry-low": {
       nombre: "Adidas Rivalry Low",
@@ -70,6 +70,54 @@ const zapatillas = {
       talles: "36, 37, 38, 43, 45",
       colores: "Blanco, Rojo, Negro",
       url: "https://images.puma.com/image/upload/f_auto,q_auto,w_2000,b_rgb:FAFAFA/global/401534/01/sv01/fnd/ARG/fmt/png"
+    },
+    "new-balance-574-core": {
+      nombre: "New Balance 574 Core",
+      precio: "$199",
+      descripcion: "La 574 fue diseñada con el objetivo de ser versátil y una muestra de innovación que desafía los límites y puede ser usada en una gran variedad de situaciones.",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://www.newbalance.com.ar/on/demandware.static/-/Sites-siteCatalog_NBEU/default/dwc8c8cad2/new_images/N1T000195155/N1T000195155-IMAGE-1.webp"
+    },
+    "new-balance-530": {
+      nombre: "New Balance 530",
+      precio: "$199",
+      descripcion: "La 530 combina la estética clásica, con la fiabilidad de una zapatilla de running de alto rendimiento y que aplica un estilo contemporáneo y cotidiano.",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://www.newbalance.com.ar/on/demandware.static/-/Sites-siteCatalog_NBEU/default/dwe7bff167/new_images/N1T000448911/N1T000448911-IMAGE-1.webp"
+    },
+    "new-balance-u574": {
+      nombre: "New Balance U574",
+      precio: "$199",
+      descripcion: "Con un diseño híbrido para asfalto y trail, la U574 ofrece una exclusiva combinación de versatilidad, novedad, sencillez, resistencia, durabilidad y confort",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://www.newbalance.com.ar/on/demandware.static/-/Sites-siteCatalog_NBEU/default/dw9b9d6ca1/new_images/N10198006128/N10198006128-IMAGE-1.webp"
+    },
+    "topper-hyde-ii": {
+      nombre: "Topper HYDE II",
+      precio: "$199",
+      descripcion: "Nueva versión de la clásica Hyde, con detalles renovados que le aportan mas identidad y sigue siendo la opción de lona unisex mas canchera.",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://topperarg.vtexassets.com/arquivos/ids/386097-800-800"
+    },
+    "topper-gondor-ii": {
+      nombre: "Topper GONDOR II",
+      precio: "$199",
+      descripcion: "Segunda versión de un clasico de los deportes Outdoor. Ideal para desarrollar todo tipo de actividades al aire libre y en la ciudad.",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://topperarg.vtexassets.com/arquivos/ids/466420-800-800"
+    },
+    "topper-vallarta": {
+      nombre: "Topper VALLARTA",
+      precio: "$199",
+      descripcion: "Este nuevo modelo combina el estilo del día a día con el toque retro de los ´90s, con un diseño moderno y materiales durables.",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://topperarg.vtexassets.com/arquivos/ids/463000-800-800"
     }
   };
 const $zapatillas = document.querySelectorAll('[data-zapatilla]');
