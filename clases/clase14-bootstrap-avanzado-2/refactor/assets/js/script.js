f364830f6214b59c55982ef227437997a23329b1
GuillermoCochrane <guilleac81@gmail.com>
2025-09-11 02:02:46 -0300

    Base del proyecto para refactor

---
+++ clases/clase14-bootstrap-avanzado-2/refactor/assets/js/script.js
@@ -0,0 +1,143 @@
+const zapatillas = {
+    "nike-dunk-low": {
+    nombre: "Nike Dunk Low",
+    precio: "$199",
+    descripcion: "El icono del basquetbol de los 80, que se creó para la cancha pero conquistó las calles del mundo.",
+    talles: "36, 37, 38, 43, 45",
+    colores: "Blanco, Rojo, Negro",
+    url: "https://nikearprod.vtexassets.com/arquivos/ids/378649-800-800"
+    },
+    "air-jordan-1-low": {
+      nombre: "Air Jordan 1 Low",
+      precio: "$199",
+      descripcion: "La unidad encapsulada Air-Sole proporciona amortiguación ligera y un diseño de alta calidad para mayor comodidad.",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://nikearprod.vtexassets.com/arquivos/ids/981162-800-800"
+    },
+    "nike-pegasus-40": {
+      nombre: "Nike Pegasus 40",
+      precio: "$199",
+      descripcion: "Con una pisada elástica para cada tipo de carrera, vuelve la sensación ideal para ayudarte a alcanzar tus objetivos",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://nikearprod.vtexassets.com/arquivos/ids/701565-800-800"
+    },
+    "adidas-rivalry-low": {
+      nombre: "Adidas Rivalry Low",
+      precio: "$199",
+      descripcion: "Las Rivalry son una leyenda en las canchas, pero ademas representan un modelo versátil para cualquier estilo de vida.",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/0f108327ea874f9b93fdaf20014a38e5_9366/Zapatillas_Rivalry_Low_Negro_FZ6327_01_00_standard.jpg"
+    },
+    "adidas-gazelle": {
+      nombre: "Adidas Gazelle",
+      precio: "$199",
+      descripcion: "Las Gazelle nacieron como zapatillas de training versátiles, pero se convertirsieron en un clásico de la moda urbana",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/0748ddb37db24595abf4a80b00ee643d_9366/Zapatillas_Gazelle_UNISEX_Negro_BB2507_01_standard.jpg"
+    },
+    "adidas-forum-84-low": {
+      nombre: "Adidas Forum 84 Low",
+      precio: "$199",
+      descripcion: "Desde 1984 su diseño vanguardista fue icónico. Siguen evolucionando al ser reimaginadas para el skate",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/d5c2891dd9c24b56b6d6cdb6bc438f8b_9366/Zapatillas_Forum_84_Low_ADV_Blanco_JP6094_01_00_standard.jpg"
+    },
+    "puma-pounce-lite": {
+      nombre: "Puma Pounce Lite",
+      precio: "$199",
+      descripcion: "Las Pounce Lite te ofrecen comodidad durante todo el día y en cualquier ocasión. Perfectas para tu vida activa, combinan confort y diseño moderno.",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://images.puma.com/image/upload/f_auto,q_auto,w_2000,b_rgb:FAFAFA/global/312033/01/sv01/fnd/ARG/fmt/png"
+    },
+    "puma-suede-xl": {
+      nombre: "Puma Suede XL",
+      precio: "$199",
+      descripcion: "Esta nueva versión de las Suede se inspira en la herencia de PUMA en el mundo del breakdance y en su influencia en el streetwear moderno, conservando su ADN icónico",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://images.puma.com/image/upload/f_auto,q_auto,w_2000,b_rgb:FAFAFA/global/395205/02/sv01/fnd/ARG/fmt/png"
+    },
+    "puma-speedcat-cuero": {
+      nombre: "Puma Speedcat Cuero",
+      precio: "$199",
+      descripcion: "Las Speedca destacan entre la multitud por su forma inspirada en las zapatillas de carreras y sus líneas elegantes, que le confieren un aspecto rápido y atrevido",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://images.puma.com/image/upload/f_auto,q_auto,w_2000,b_rgb:FAFAFA/global/401534/01/sv01/fnd/ARG/fmt/png"
+    },
+    "new-balance-574-core": {
+      nombre: "New Balance 574 Core",
+      precio: "$199",
+      descripcion: "La 574 fue diseñada con el objetivo de ser versátil y una muestra de innovación que desafía los límites y puede ser usada en una gran variedad de situaciones.",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://www.newbalance.com.ar/on/demandware.static/-/Sites-siteCatalog_NBEU/default/dwc8c8cad2/new_images/N1T000195155/N1T000195155-IMAGE-1.webp"
+    },
+    "new-balance-530": {
+      nombre: "New Balance 530",
+      precio: "$199",
+      descripcion: "La 530 combina la estética clásica, con la fiabilidad de una zapatilla de running de alto rendimiento y que aplica un estilo contemporáneo y cotidiano.",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://www.newbalance.com.ar/on/demandware.static/-/Sites-siteCatalog_NBEU/default/dwe7bff167/new_images/N1T000448911/N1T000448911-IMAGE-1.webp"
+    },
+    "new-balance-u574": {
+      nombre: "New Balance U574",
+      precio: "$199",
+      descripcion: "Con un diseño híbrido para asfalto y trail, la U574 ofrece una exclusiva combinación de versatilidad, novedad, sencillez, resistencia, durabilidad y confort",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://www.newbalance.com.ar/on/demandware.static/-/Sites-siteCatalog_NBEU/default/dw9b9d6ca1/new_images/N10198006128/N10198006128-IMAGE-1.webp"
+    },
+    "topper-hyde-ii": {
+      nombre: "Topper HYDE II",
+      precio: "$199",
+      descripcion: "Nueva versión de la clásica Hyde, con detalles renovados que le aportan mas identidad y sigue siendo la opción de lona unisex mas canchera.",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://topperarg.vtexassets.com/arquivos/ids/386097-800-800"
+    },
+    "topper-gondor-ii": {
+      nombre: "Topper GONDOR II",
+      precio: "$199",
+      descripcion: "Segunda versión de un clasico de los deportes Outdoor. Ideal para desarrollar todo tipo de actividades al aire libre y en la ciudad.",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://topperarg.vtexassets.com/arquivos/ids/466420-800-800"
+    },
+    "topper-vallarta": {
+      nombre: "Topper VALLARTA",
+      precio: "$199",
+      descripcion: "Este nuevo modelo combina el estilo del día a día con el toque retro de los ´90s, con un diseño moderno y materiales durables.",
+      talles: "36, 37, 38, 43, 45",
+      colores: "Blanco, Rojo, Negro",
+      url: "https://topperarg.vtexassets.com/arquivos/ids/463000-800-800"
+    }
+  };
+const $zapatillas = document.querySelectorAll('[data-zapatilla]');
+for (const zapatilla of $zapatillas) {
+  zapatilla.addEventListener('click', function(event) {
+    event.preventDefault();
+    const modalName = document.querySelector('#modal-nombre');
+    const modalPrecio = document.querySelector('#modal-precio');
+    const modalDescripcion = document.querySelector('#modal-descripcion');
+    const modalTalles = document.querySelector('#modal-talles');
+    const modalColores = document.querySelector('#modal-colores');
+    const modalImg = document.querySelector('#modal-img');
+    var id = this.getAttribute('data-zapatilla');
+    const currentZapatilla = zapatillas[id];
+    modalName.textContent = currentZapatilla.nombre;
+    modalPrecio.textContent = currentZapatilla.precio;
+    modalDescripcion.textContent = currentZapatilla.descripcion;
+    modalTalles.textContent = currentZapatilla.talles;
+    modalColores.textContent = currentZapatilla.colores;
+    modalImg.src = currentZapatilla.url;
+    modalImg.alt = currentZapatilla.nombre;
+  });
+};
