dddcccc57ad09f7abfa55c465078fa5556a4b179
GuillermoCochrane <guilleac81@gmail.com>
2025-09-12 19:57:06 -0300

    Agregada función que pone la primer letra de cada palabra en mayúscula

--- clases/clase14-bootstrap-avanzado-2/refactor/assets/js/utilities/utilities.js
+++ clases/clase14-bootstrap-avanzado-2/refactor/assets/js/utilities/utilities.js
@@ -2,4 +2,11 @@ import { zapatillas as productos } from '../../data/DB.js';
 
 export function filterProduct(key, value) {
   return productos.filter(productos => productos[key].toLowerCase() === value.toLowerCase());
+};
+
+export function capitalizeWords(string) {
+  // \b - Detecta el inicio o fin de una palabra, 
+  // \w - Detecta cualquier caracter alfanumérico,
+  // /g - global (busca en toda la cadena)
+  return string.replace(/\b\w/g, char => char.toUpperCase());
 }
