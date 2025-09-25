37387564835e3820948af12ed807f62e6deeb815
GuillermoCochrane <guilleac81@gmail.com>
2025-09-25 20:54:09 -0300

    Puesta en funcionamiento del Modal (MVP)

--- clases/clase16-fetch/desafio_pokemon/assets/js/core/app.js
+++ clases/clase16-fetch/desafio_pokemon/assets/js/core/app.js
@@ -1,11 +1,11 @@
 import { createCardSection } from '../components/components.js';
-/* import { modalHandler } from './modalHandler.js'; */
+import { modalHandler } from './modalHandler.js';
 import { dataFetcher } from './dataFetcher.js';
 
 async function createApp() {
     const pokemons = await dataFetcher();
     createCardSection(pokemons);
-/*     modalHandler(); */
+    modalHandler(pokemons);
 }
 
 document.addEventListener('DOMContentLoaded', createApp);
--- clases/clase16-fetch/desafio_pokemon/assets/js/core/modalHandler.js
+++ clases/clase16-fetch/desafio_pokemon/assets/js/core/modalHandler.js
@@ -1,31 +1,24 @@
-import { zapatillas } from '../../data/DB.js';
 import {$} from '../utilities/dom.js';
 
-export function modalHandler() {
-  const $zapatillas = document.querySelectorAll('[data-zapatilla]');
-  for (const $zapatilla of $zapatillas) {
-    $zapatilla.addEventListener('click', function(event) {
+export function modalHandler(data) {
+  const $pokemonBTN = document.querySelectorAll('[data-pokemon]');
+  for (const $pokeBtn of $pokemonBTN) {
+    $pokeBtn.addEventListener('click', function(event) {
       event.preventDefault();
       const $modalName = $('#modal-nombre');
-      const $modalPrecio = $('#modal-precio');
-      const $modalDescripcion = $('#modal-descripcion');
-      const $modalTalles = $('#modal-talles');
-      const $modalColores = $('#modal-colores');
       const $modalImg = $('#modal-img');
-      let id = this.getAttribute('data-zapatilla');$
-      let currentZapatilla = "";
-      for (const zapatilla of zapatillas) {
-        if (zapatilla.id == id) {
-          currentZapatilla = zapatilla;
+
+      let id = this.getAttribute('data-pokemon');
+      let currentPokemon = "";
+      for (const pokemon of data) {
+        if (pokemon.id == id) {
+          currentPokemon = pokemon;
+          break;
         }
       }
-      $modalName.textContent = currentZapatilla.nombre;
-      $modalPrecio.textContent = currentZapatilla.precio;
-      $modalDescripcion.textContent = currentZapatilla.descripcion;
-      $modalTalles.textContent = currentZapatilla.talles;
-      $modalColores.textContent = currentZapatilla.colores;
-      $modalImg.src = currentZapatilla.url;
-      $modalImg.alt = currentZapatilla.nombre;
+      $modalName.textContent = currentPokemon.name;
+      $modalImg.setAttribute('src', currentPokemon.sprites.front_default);
+      $modalImg.setAttribute('alt', currentPokemon.name);
     });
   };
 }
