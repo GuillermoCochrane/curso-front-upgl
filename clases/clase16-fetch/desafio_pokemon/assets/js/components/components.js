e330bdef22de88ee56949f8b92513543167a80c1
GuillermoCochrane <guilleac81@gmail.com>
2025-09-26 18:49:46 -0300

    Agregado componente que genera la sección de tipos de  la card

--- clases/clase16-fetch/desafio_pokemon/assets/js/components/components.js
+++ clases/clase16-fetch/desafio_pokemon/assets/js/components/components.js
@@ -1,7 +1,6 @@
 import { $, createElement, createImage, createButton  } from '../utilities/dom.js';
 
 // Crea componente del header de la tarjeta del Pokemon
-
 export function createCardHeader(id) {
     const $cardHeader = createElement('header', 'card-header text-center');
     const $span = createElement('h5', 'text-muted fw-semibold', `#${id}`);
@@ -9,12 +8,23 @@ export function createCardHeader(id) {
     return $cardHeader;
 }
 
+// Crea componente de los tipos del Pokemon
+export function createCardTypesBadges(types) {
+    const $cardTypes = createElement('section', 'd-flex justify-content-center flex-wrap gap-2 mb-3');
+    for (const type of types) {
+        const $badge = createElement('span', 'badge p-2 mx-1 text-center', type.type.name, false, null, [type], true);
+        $cardTypes.append($badge);
+    }
+    return $cardTypes;
+}
+
 // Crea componente de datos del Pokemon
-export function createCardInfo(nombre,id) {
-    const $cardInfo = createElement('div', 'card-body');
-    const $title = createElement('h5', 'card-title product-card-title',  nombre ? nombre : ' Pokemon Generico', false);
+export function createCardInfo(nombre, id, tipos) {
+    const $cardInfo = createElement('div', 'card-body text-center');
+    const $title = createElement('h5', 'card-title product-card-title fw-bold text-capitalize fs-4',  nombre ? nombre : ' Pokemon Generico', false);
+    const $typeContainer = createCardTypesBadges(tipos);
     const $button = createButton('Ver Detalles', 'btn btn-outline-dark', null, 'modal', '#detallesModal',  id ? id : 'pokemon-generico');
-    $cardInfo.append($title, $button);
+    $cardInfo.append($title,$typeContainer, $button);
     return $cardInfo;
 }
 
@@ -23,7 +33,7 @@ export function createProductCard(pokemon) {
     const $productCard = createElement('article', 'card product-card m-3', null, false, null, pokemon.types);
     const $header = createCardHeader(pokemon.id);
     const $image = createImage(pokemon.sprites ? pokemon.sprites.front_default : './assets/img/default.png', pokemon.name ? pokemon.name : 'Pokemon Génerico', 'card-img-top');
-    const $cardInfo = createCardInfo(pokemon.name, pokemon.id);
+    const $cardInfo = createCardInfo(pokemon.name, pokemon.id, pokemon.types);
     $productCard.append($header,$image, $cardInfo);
     return $productCard;
 }
