0e285947f9dec1498d58725bfc9f99a91e943b01
GuillermoCochrane <guilleac81@gmail.com>
2025-09-26 01:18:32 -0300

    Ejercicio hecho en la clase 16

---
+++ clases/clase16-fetch/pokemon.js
@@ -0,0 +1,23 @@
+function traerPokemons() {
+  fetch('https://pokeapi.co/api/v2/pokemon')
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      const pokemons = data.results;
+      console.log(pokemons);
+      generarListado(pokemons);
+    })
+    .catch(error => console.log(error));
+}
+
+function generarListado(pokemons) {
+  const listado = document.getElementById('pokemons');
+  const counter = 0
+  for (const pokemon of pokemons) {
+    counter++;
+    const li = `<li class="list-group-item">#${counter} - ${pokemon.name}</li>`;
+    listado.innerHTML += li;    
+  }
+
+}
