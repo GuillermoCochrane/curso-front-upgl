a478578aeb138b14107fa13dd7a8ca0538a35ce0
GuillermoCochrane <guilleac81@gmail.com>
2025-09-27 04:50:14 -0300

    Implementado infinite scroll

--- clases/clase16-fetch/desafio_pokemon/assets/js/core/app.js
+++ clases/clase16-fetch/desafio_pokemon/assets/js/core/app.js
@@ -1,11 +1,13 @@
 import { createCardSection } from '../components/components.js';
 import { modalHandler } from './modalHandler.js';
 import { dataFetcher } from './dataFetcher.js';
+import { infiniteScrollHandler } from './infiniteScrollHandler.js';
 
 async function createApp() {
     const pokemons = await dataFetcher();
     createCardSection(pokemons);
     modalHandler(pokemons);
+    infiniteScrollHandler();
 }
 
 document.addEventListener('DOMContentLoaded', createApp);
