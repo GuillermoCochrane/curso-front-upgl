5f5cf2dfdd5e8718f4a00e6cf8e5a0711e9e9d30
GuillermoCochrane <guilleac81@gmail.com>
2025-09-27 21:35:00 -0300

    Agregada función que carga los datos de las stats del pokemos

--- clases/clase16-fetch/desafio_pokemon/assets/js/core/modalHandler.js
+++ clases/clase16-fetch/desafio_pokemon/assets/js/core/modalHandler.js
@@ -23,6 +23,7 @@ export function modalHandler() {
 function loadModalData(pokemon) {
     modalHeaderData(pokemon.id, pokemon.name, pokemon.types);
     modalCarouselData(pokemon.sprites, pokemon.name);
+    modalStatsData(pokemon.stats, pokemon.height, pokemon.weight);
 }
 
 // Función que carga los datos del header del modal
@@ -61,4 +62,37 @@ function modalCarouselData(sprites, name) {
   $official.src = sprites.other['official-artwork'].front_default;
   $official.alt = textData.official + name;
   $official.title = textData.official + name;
+}
+
+// Función que carga las stats del modal
+function modalStatsData(stats, height, weight) {
+  const $hp = $('#stat-hp');
+  const $hpValue = $('#stat-hp-value');
+  const $atk = $('#stat-attack');
+  const $atkValue = $('#stat-attack-value');
+  const $def = $('#stat-defense');
+  const $defValue = $('#stat-defense-value');
+  const $spd = $('#stat-speed');
+  const $spdValue = $('#stat-speed-value');
+  const $satk = $('#stat-special-attack');
+  const $satkValue = $('#stat-special-attack-value');
+  const $sdef = $('#stat-special-defense');
+  const $sdefValue = $('#stat-special-defense-value');
+  const $height = $('#modal-height');
+  const $weight = $('#modal-weight');
+
+  $hp.value = stats[0].base_stat;
+  $hpValue.textContent = stats[0].base_stat;
+  $atk.value = stats[1].base_stat;
+  $atkValue.textContent = stats[1].base_stat;
+  $def.value = stats[2].base_stat;
+  $defValue.textContent = stats[2].base_stat;
+  $spd.value = stats[5].base_stat; 
+  $spdValue.textContent = stats[5].base_stat;
+  $satk.value = stats[3].base_stat;
+  $satkValue.textContent = stats[3].base_stat;
+  $sdef.value = stats[4].base_stat;
+  $sdefValue.textContent = stats[4].base_stat;
+  $height.textContent = `${height / 10} m`;
+  $weight.textContent = `${weight / 10} kg`;
 }
