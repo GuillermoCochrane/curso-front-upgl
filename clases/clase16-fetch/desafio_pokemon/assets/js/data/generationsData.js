2d8bbb7d3cd6bea1b3389da458dc2b7fbe4bd11a
GuillermoCochrane <guilleac81@gmail.com>
2025-09-30 18:22:16 -0300

    DB auxiliar de generaciones

---
+++ clases/clase16-fetch/desafio_pokemon/assets/js/data/generationsData.js
@@ -0,0 +1,47 @@
+export const generations  = [
+  {
+    id: "generation-i",
+    name: "Generación I",
+    versions: ["red-blue", "yellow"]
+  },
+  {
+    id: "generation-ii", 
+    name: "Generación II",
+    versions: ["gold-silver", "crystal"]
+  },
+  {
+    id: "generation-iii",
+    name: "Generación III", 
+    versions: ["ruby-sapphire", "emerald", "firered-leafgreen"]
+  },
+  {
+    id: "generation-iv",
+    name: "Generación IV",
+    versions: ["diamond-pearl", "platinum", "heartgold-soulsilver"]
+  },
+  {
+    id: "generation-v",
+    name: "Generación V",
+    versions: ["black-white", "black-2-white-2"]
+  },
+  {
+    id: "generation-vi",
+    name: "Generación VI",
+    versions: ["x-y", "omega-ruby-alpha-sapphire"]
+  },
+  {
+    id: "generation-vii",
+    name: "Generación VII",
+    versions: ["sun-moon", "ultra-sun-ultra-moon", "lets-go-pikachu-lets-go-eevee"]
+  },
+  {
+    id: "generation-viii", 
+    name: "Generación VIII",
+    versions: ["sword-shield"]
+  },
+  {
+    id: "generation-ix",
+    name: "Generación IX", 
+    versions: ["scarlet-violet"]
+  }
+];
