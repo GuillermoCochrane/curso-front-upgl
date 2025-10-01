5ad0b5b6b9361f83da88c80b94078f3264adf8fc
GuillermoCochrane <guilleac81@gmail.com>
2025-10-01 17:14:13 -0300

    Agregada DB auxiliar con casos de nombres problemáticos

--- clases/clase16-fetch/desafio_pokemon/assets/js/data/generationsData.js
+++ clases/clase16-fetch/desafio_pokemon/assets/js/data/generationsData.js
@@ -45,3 +45,12 @@ export const generations  = [
     versions: ["scarlet-violet"]
   }
 ];
+
+//nombres problematicos de versiones
+export const versionDisplayNames = {
+  "black-2-white-2": "Black 2 / White 2",
+  "ultra-sun-ultra-moon": "Ultra Sun / Ultra Moon",
+  "lets-go-pikachu-lets-go-eevee": "Let's Go Pikachu / Let's Go Eevee",
+  "omega-ruby-alpha-sapphire": "Omega Ruby / Alpha Sapphire",
+  "x-y": "X & Y"
+};
