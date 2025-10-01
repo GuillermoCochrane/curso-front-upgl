5f86fe952b57436008ffdebba1cbd3182ae1eb6d
GuillermoCochrane <guilleac81@gmail.com>
2025-10-01 17:15:22 -0300

    Funcionalidad para corregir nombres problemáticos

--- clases/clase16-fetch/desafio_pokemon/assets/js/utilities/formatData.js
+++ clases/clase16-fetch/desafio_pokemon/assets/js/utilities/formatData.js
@@ -1,3 +1,5 @@
+import { versionDisplayNames } from '../data/generationsData.js';
+
 export function formatText(name, separator = ' ') {
   return name.split('-').map(word => 
     word.charAt(0).toUpperCase() + word.slice(1)
@@ -6,4 +8,15 @@ export function formatText(name, separator = ' ') {
 
 export function formatMoveLevel(level) {
   return level > 0 ? level : '-';
+}
+
+// Función mejorada para formatear versiones
+export function formatVersionName(version) {
+  // Si existe en el mapeo, usar ese nombre
+  if (versionDisplayNames[version]) {
+    return versionDisplayNames[version];
+  }
+  
+  // Si no, usar formatText normal
+  return formatText(version, ' / ');
 }
