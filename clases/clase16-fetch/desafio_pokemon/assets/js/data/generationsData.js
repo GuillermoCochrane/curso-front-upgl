edcdd22c4af9f4ea9b03dd0b5be2ffe9002d2390
GuillermoCochrane <guilleac81@gmail.com>
2025-10-02 22:17:43 -0300

    Agregada DB axuiliar de con datos separados x juegos

--- clases/clase16-fetch/desafio_pokemon/assets/js/data/generationsData.js
+++ clases/clase16-fetch/desafio_pokemon/assets/js/data/generationsData.js
@@ -54,3 +54,26 @@ export const versionDisplayNames = {
   "omega-ruby-alpha-sapphire": "Omega Ruby / Alpha Sapphire",
   "x-y": "X & Y"
 };
+
+//Datos estructurados por juegos
+export const games = [
+  { id: "red-blue", name: "Red / Blue", generation: "i", color: "#ff0000" },
+  { id: "yellow", name: "Yellow", generation: "i", color: "#ffff00" },
+  { id: "gold-silver", name: "Gold / Silver", generation: "ii", color: "#ffd700" },
+  { id: "crystal", name: "Crystal", generation: "ii", color: "#00ffff" },
+  { id: "ruby-sapphire", name: "Ruby / Sapphire", generation: "iii", color: "#ff0000" },
+  { id: "emerald", name: "Emerald", generation: "iii", color: "#00ff00" },
+  { id: "firered-leafgreen", name: "FireRed / LeafGreen", generation: "iii", color: "#ff4500" },
+  { id: "diamond-pearl", name: "Diamond / Pearl", generation: "iv", color: "#b19cd9" },
+  { id: "platinum", name: "Platinum", generation: "iv", color: "#e5e4e2" },
+  { id: "heartgold-soulsilver", name: "HeartGold / SoulSilver", generation: "iv", color: "#ffd700" },
+  { id: "black-white", name: "Black / White", generation: "v", color: "#000000" },
+  { id: "black-2-white-2", name: "Black 2 / White 2", generation: "v", color: "#36454f" },
+  { id: "x-y", name: "X & Y", generation: "vi", color: "#007acc" },
+  { id: "omega-ruby-alpha-sapphire", name: "Omega Ruby / Alpha Sapphire", generation: "vi", color: "#ff0000" },
+  { id: "sun-moon", name: "Sun/Moon", generation: "vii", color: "#ff8c00" },
+  { id: "ultra-sun-ultra-moon", name: "Ultra Sun / Ultra Moon", generation: "vii", color: "#8a2be2" },
+  { id: "lets-go-pikachu-lets-go-eevee", name: "Let's Go Pikachu / Let's Go Eevee", generation: "vii", color: "#ffd700" },
+  { id: "sword-shield", name: "Sword / Shield", generation: "viii", color: "#0000ff" },
+  { id: "scarlet-violet", name: "Scarlet / Violet", generation: "ix", color: "#ff2400" }
+];
