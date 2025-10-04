8edf8cba10f5e0edac5ff84858f44f9e38545452
GuillermoCochrane <guilleac81@gmail.com>
2025-10-04 19:59:17 -0300

    DB modificada para agregar color de fuente a cada juego

--- clases/clase16-fetch/desafio_pokemon/assets/js/data/generationsData.js
+++ clases/clase16-fetch/desafio_pokemon/assets/js/data/generationsData.js
@@ -1,49 +1,53 @@
-export const generations  = [
+export const generations = [
   {
     id: "generation-i",
     name: "Generación I",
-    versions: ["red-blue", "yellow"]
+    versions: ["red-blue", "yellow"],
   },
   {
-    id: "generation-ii", 
+    id: "generation-ii",
     name: "Generación II",
-    versions: ["gold-silver", "crystal"]
+    versions: ["gold-silver", "crystal"],
   },
   {
     id: "generation-iii",
-    name: "Generación III", 
-    versions: ["ruby-sapphire", "emerald", "firered-leafgreen"]
+    name: "Generación III",
+    versions: ["ruby-sapphire", "emerald", "firered-leafgreen"],
   },
   {
     id: "generation-iv",
     name: "Generación IV",
-    versions: ["diamond-pearl", "platinum", "heartgold-soulsilver"]
+    versions: ["diamond-pearl", "platinum", "heartgold-soulsilver"],
   },
   {
     id: "generation-v",
     name: "Generación V",
-    versions: ["black-white", "black-2-white-2"]
+    versions: ["black-white", "black-2-white-2"],
   },
   {
     id: "generation-vi",
     name: "Generación VI",
-    versions: ["x-y", "omega-ruby-alpha-sapphire"]
+    versions: ["x-y", "omega-ruby-alpha-sapphire"],
   },
   {
     id: "generation-vii",
     name: "Generación VII",
-    versions: ["sun-moon", "ultra-sun-ultra-moon", "lets-go-pikachu-lets-go-eevee"]
+    versions: [
+      "sun-moon",
+      "ultra-sun-ultra-moon",
+      "lets-go-pikachu-lets-go-eevee",
+    ],
   },
   {
-    id: "generation-viii", 
+    id: "generation-viii",
     name: "Generación VIII",
-    versions: ["sword-shield"]
+    versions: ["sword-shield"],
   },
   {
     id: "generation-ix",
-    name: "Generación IX", 
-    versions: ["scarlet-violet"]
-  }
+    name: "Generación IX",
+    versions: ["scarlet-violet"],
+  },
 ];
 
 //nombres problematicos de versiones
@@ -52,28 +56,142 @@ export const versionDisplayNames = {
   "ultra-sun-ultra-moon": "Ultra Sun / Ultra Moon",
   "lets-go-pikachu-lets-go-eevee": "Let's Go Pikachu / Let's Go Eevee",
   "omega-ruby-alpha-sapphire": "Omega Ruby / Alpha Sapphire",
-  "x-y": "X & Y"
+  "x-y": "X & Y",
 };
 
 //Datos estructurados por juegos
 export const games = [
-  { id: "red-blue", name: "Red / Blue", generation: "i", color: "#ff0000" },
-  { id: "yellow", name: "Yellow", generation: "i", color: "#ffff00" },
-  { id: "gold-silver", name: "Gold / Silver", generation: "ii", color: "#ffd700" },
-  { id: "crystal", name: "Crystal", generation: "ii", color: "#00ffff" },
-  { id: "ruby-sapphire", name: "Ruby / Sapphire", generation: "iii", color: "#ff0000" },
-  { id: "emerald", name: "Emerald", generation: "iii", color: "#00ff00" },
-  { id: "firered-leafgreen", name: "FireRed / LeafGreen", generation: "iii", color: "#ff4500" },
-  { id: "diamond-pearl", name: "Diamond / Pearl", generation: "iv", color: "#b19cd9" },
-  { id: "platinum", name: "Platinum", generation: "iv", color: "#e5e4e2" },
-  { id: "heartgold-soulsilver", name: "HeartGold / SoulSilver", generation: "iv", color: "#ffd700" },
-  { id: "black-white", name: "Black / White", generation: "v", color: "#000000" },
-  { id: "black-2-white-2", name: "Black 2 / White 2", generation: "v", color: "#36454f" },
-  { id: "x-y", name: "X & Y", generation: "vi", color: "#007acc" },
-  { id: "omega-ruby-alpha-sapphire", name: "Omega Ruby / Alpha Sapphire", generation: "vi", color: "#ff0000" },
-  { id: "sun-moon", name: "Sun/Moon", generation: "vii", color: "#ff8c00" },
-  { id: "ultra-sun-ultra-moon", name: "Ultra Sun / Ultra Moon", generation: "vii", color: "#8a2be2" },
-  { id: "lets-go-pikachu-lets-go-eevee", name: "Let's Go Pikachu / Let's Go Eevee", generation: "vii", color: "#ffd700" },
-  { id: "sword-shield", name: "Sword / Shield", generation: "viii", color: "#0000ff" },
-  { id: "scarlet-violet", name: "Scarlet / Violet", generation: "ix", color: "#ff2400" }
+  { 
+    id: "red-blue",
+    name: "Red / Blue",
+    generation: "i",
+    color: "#ff0000",
+    font: "--light-font"
+  },
+  { 
+    id: "yellow",
+    name: "Yellow",
+    generation: "i",
+    color: "#ffff00",
+    font: "--dark-font"
+  },
+  {
+    id: "gold-silver",
+    name: "Gold / Silver",
+    generation: "ii",
+    color: "#ffd700",
+    font: "--dark-font"
+  },
+  { 
+    id: "crystal",
+    name: "Crystal", 
+    generation: "ii", 
+    color: "#00ffff",
+    font: "--dark-font"
+  },
+  {
+    id: "ruby-sapphire",
+    name: "Ruby / Sapphire",
+    generation: "iii",
+    color: "#ff0000",
+    font: "--light-font"
+  },
+  { 
+    id: "emerald",
+    name: "Emerald",
+    generation: "iii",
+    color: "#00ff00",
+    font: "--dark-font"
+  },
+  {
+    id: "firered-leafgreen",
+    name: "FireRed / LeafGreen",
+    generation: "iii",
+    color: "#ff4500",
+    font: "--light-font"
+  },
+  {
+    id: "diamond-pearl",
+    name: "Diamond / Pearl",
+    generation: "iv",
+    color: "#b19cd9",
+    font: "--dark-font"
+  },
+  { 
+    id: "platinum",
+    name: "Platinum",
+    generation: "iv",
+    color: "#e5e4e2",
+    font: "--dark-font"
+  },
+  {
+    id: "heartgold-soulsilver",
+    name: "HeartGold / SoulSilver",
+    generation: "iv",
+    color: "#ffd700",
+    font: "--dark-font"
+  },
+  {
+    id: "black-white",
+    name: "Black / White",
+    generation: "v",
+    color: "#000000",
+    font: "--light-font"
+  },
+  {
+    id: "black-2-white-2",
+    name: "Black 2 / White 2",
+    generation: "v",
+    color: "#36454f",
+    font: "--light-font"
+  },
+  { 
+    id: "x-y",
+    name: "X & Y",
+    generation: "vi",
+    color: "#007acc",
+    font: "--light-font"
+  },
+  {
+    id: "omega-ruby-alpha-sapphire",
+    name: "Omega Ruby / Alpha Sapphire",
+    generation: "vi",
+    color: "#ff0000",
+    font: "--light-font"
+  },
+  { 
+    id: "sun-moon",
+    name: "Sun/Moon", 
+    generation: "vii",
+    color: "#ff8c00",
+    font: "--light-font"
+  },
+  {
+    id: "ultra-sun-ultra-moon",
+    name: "Ultra Sun / Ultra Moon",
+    generation: "vii",
+    color: "#8a2be2",
+    font: "--light-font"
+  },
+  {
+    id: "lets-go-pikachu-lets-go-eevee",
+    name: "Let's Go Pikachu / Let's Go Eevee",
+    generation: "vii",
+    color: "#ffd700",
+    font: "--dark-font"
+  },
+  {
+    id: "sword-shield",
+    name: "Sword / Shield",
+    generation: "viii",
+    color: "#0000ff",
+    font: "--light-font"
+  },
+  {
+    id: "scarlet-violet",
+    name: "Scarlet / Violet",
+    generation: "ix",
+    color: "#ff2400",
+    font: "--light-font"
+  },
 ];
