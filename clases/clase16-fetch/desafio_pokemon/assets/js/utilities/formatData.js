31320a97057fc68ed68bcca5be0df51325f3bc7f
GuillermoCochrane <guilleac81@gmail.com>
2025-10-07 04:04:26 -0300

    Agregada función auxiliar para ordenar por nivel (que se encuetra dentro del objeto método)

--- clases/clase16-fetch/desafio_pokemon/assets/js/utilities/formatData.js
+++ clases/clase16-fetch/desafio_pokemon/assets/js/utilities/formatData.js
@@ -22,21 +22,38 @@ export function formatVersionName(version) {
 }
 
 // Función para ordenar un array de objetos
-export function ordenarArray(array, columna, orden = 'asc') {
-  return [...array].sort((a, b) => {
-    let valorA = a[columna];
-    let valorB = b[columna];
+export function arraySorter(array, column, ascending = true) {
+  const sortedArray = array.sort((a, b) => {
+    let valueA = a[column];
+    let valueb = b[column];
     
-    // CASO ESPECIAL: Si ordenamos por nivel
-    if (columna === 'level') {
-      valorA = obtenerNivelMasBajo(a.methods);
-      valorB = obtenerNivelMasBajo(b.methods);
+    // CASO ESPECIAL: Si orderamos por nivel
+    if (column === 'level') {
+      valueA = getLowerLevel(a.methods);
+      valueb = getLowerLevel(b.methods);
     }
     
-    if (orden === 'asc') {
-      return valorA < valorB ? -1 : 1;
+    if (ascending === true) {
+      return valueA < valueb ? -1 : 1;
     } else {
-      return valorA > valorB ? -1 : 1;
+      return valueA > valueb ? -1 : 1;
     }
   });
+  return sortedArray;
+}
+
+// Función helper para sacar el nivel más bajo
+function getLowerLevel(methods) {
+  // 1. Extraer TODOS los niveles
+  const levels = methods.map(m => m.level);
+  
+  // 2. Filtrar solo niveles > 0 (quitamos machine/tutor/egg)
+  const positiveLevels = levels.filter(l => l > 0);
+  
+  // 3. Si hay niveles positivos, devolver el MÁS BAJO
+  if (positiveLevels.length > 0) {
+    return Math.min(...positiveLevels);
+  } else {
+    return 0; // Si no, devolver 0 (machine/tutor/egg)
+  }
 }
