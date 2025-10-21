0cbb1b21aa9ce5dbfec79bd5e872ddee53c82f1c
GuillermoCochrane <guilleac81@gmail.com>
2025-10-21 20:53:56 -0300

    Script handler del sistema de notificaciones

---
+++ clases/clase16-fetch/desafio_pokemon/assets/js/core/notificationHandler.js
@@ -0,0 +1,15 @@
+// Handler de notificaciones
+import { $ } from '../utilities/dom.js';
+
+export function showNotification(message, type = 'info') {
+  const $notification = $('#notification');
+
+  $notification.textContent = message;
+  $notification.style.borderLeftColor = `var(--bs-${type})`;
+  $notification.classList.add('show');
+  
+  setTimeout(() => {
+    $notification.classList.remove('show');
+    setTimeout(() => $notification.hidden = true, 300);
+  }, 3000);
+}
