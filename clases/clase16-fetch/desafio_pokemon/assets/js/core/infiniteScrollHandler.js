8e9c9c8414e695181acacbfc3bda5f37f0da0caa
GuillermoCochrane <guilleac81@gmail.com>
2025-10-21 19:21:02 -0300

    infiniteScrollHandler modificado, dandole funcionalidad al spinner

--- clases/clase16-fetch/desafio_pokemon/assets/js/core/infiniteScrollHandler.js
+++ clases/clase16-fetch/desafio_pokemon/assets/js/core/infiniteScrollHandler.js
@@ -1,14 +1,23 @@
-export function infiniteScrollHandler(loadMorePokemons) {
+import { hiddenToggle } from '../utilities/dom.js';
+export  function infiniteScrollHandler(loadMorePokemons) {
   let isLoading = false;
 
-  window.addEventListener('scroll', () => {
+  window.addEventListener('scroll', async () => {
     if (isLoading) return;
     
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
     
     if (scrollTop + clientHeight >= scrollHeight - 800) {
       isLoading = true;
-      loadMorePokemons().finally(() => isLoading = false);
+      hiddenToggle("spinner");
+      try {
+        await loadMorePokemons()
+      } catch(error){
+        console.error('❌ Error al cargar más Pokemon:', error);
+      } finally{
+          hiddenToggle("spinner")
+          isLoading = false;
+        };
     }
   });
 }
