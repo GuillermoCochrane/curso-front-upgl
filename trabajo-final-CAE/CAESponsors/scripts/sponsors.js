d10568bb948fc21e5a86c90459cdc43ce1dd82d9
GuillermoCochrane <guilleac81@gmail.com>
2025-10-29 03:17:19 -0300

    Agregada DB de auspiciantes de CAE

---
+++ trabajo-final-CAE/CAESponsors/scripts/sponsors.js
@@ -0,0 +1,7 @@
+const auspiciantes = [
+    { id: 1, auspiciante: "Dutto & Cheerham", logo: "dutto_cheetham.jpg" },
+    { id: 2, auspiciante: "Parrilla: La conquista de la vaca", logo: "la_conquista_de_la_vaca.jpg" },
+    { id: 3, auspiciante: "Masi Neumáticos", logo: "masi_neumaticos.jpg" },
+    { id: 4, auspiciante: "RM Automotores", logo: "RM_automotores.jpg" },
+    { id: 5, auspiciante: "Ros Sport", logo: "ros_sport.jpg" },
+];
