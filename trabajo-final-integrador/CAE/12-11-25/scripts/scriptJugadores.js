b322a617ffb00b6810ad04d3337b3b7d78426d6d
GuillermoCochrane <guilleac81@gmail.com>
2025-11-12 18:27:18 -0300

    Imágenes de jugadores reemplazada por versión webp

--- trabajo-final-integrador/CAE/12-11-25/scripts/scriptJugadores.js
+++ trabajo-final-integrador/CAE/12-11-25/scripts/scriptJugadores.js
@@ -5,37 +5,36 @@ document.addEventListener('DOMContentLoaded', () => {
     
 // Imágenes de cada jugador
 const imagenesJugadores = {
-  "Airasca Santiago": "assets/jugadores/Airasca-Santiago.jpeg",
-  "Gil Gabriel": "assets/jugadores/Gil-Gabriel.jpeg",
-  "Gonzalez Valentin": "assets/jugadores/Gonzalez-Valentin.jpeg",
-  "Andino Manuel": "assets/jugadores/Andino-Manuel.jpeg",
-  "Ferrando Marcos": "assets/jugadores/Ferrando-Marcos.jpeg",
-  "Fontana Santiago": "assets/jugadores/Fontana-Santiago.jpeg",
-  "Monzon Maximiliano": "assets/jugadores/Monzon-Maximiliano.jpeg",
-  "Velázquez Gaspar": "assets/jugadores/Velazquez-Gaspar.jpeg",
-  "Zabaleta Javier": "assets/jugadores/Zabaleta-Javier.jpeg",
-  "Rios Juan": "assets/jugadores/Rios-Juan.jpeg",
-  "Rodriguez Joaquín": "assets/jugadores/Rodriguez-Joaquin.jpeg",
-  "Montiel Gonzalo": "assets/jugadores/Montiel-Gonzalo.jpeg",
-  "Galetto Máximo": "assets/jugadores/Galetto-Maximo.jpeg",
-  "Guzmán Laureano": "assets/jugadores/Guzman-Laureano.jpeg",
-  "Arce Timoteo": "assets/jugadores/Arce-Timoteo.jpeg",
-  "Bocco Martin": "assets/jugadores/Bocco-Martin.jpeg",
-  "Cortez Fermin": "assets/jugadores/Cortez-Fermin.jpeg",
-  "Ferrando Joaquín": "assets/jugadores/Ferrando-Joaquin.jpeg",
-  "Granero Juan Pablo": "assets/jugadores/Granero-Juan-Pablo.jpeg",
-  "Reguero Franco": "assets/jugadores/Reguero-Franco.jpeg",
-  "Rodriguez Bernabe": "assets/jugadores/Rodriguez-Bernabe.jpeg",
-  "Moyano Francisco": "assets/jugadores/Moyano-Francisco.jpeg",
-  "Victorio Juan Ignacio": "assets/jugadores/Victorio-Juan-Ignacio.jpeg",
-  "Zarate Ezequiel": "assets/jugadores/Zarate-Ezequiel.jpeg",
-  "Dopazo Martin": "assets/jugadores/Dopazo-Martin.jpeg",
-  "Sanchez Bruno": "assets/jugadores/Sanchez-Bruno.jpeg",
-  "Cataldo Ramiro": "assets/jugadores/Cataldo-Ramiro.jpeg",
-  "Bruera Maximiliano": "assets/jugadores/Bruera-Maximiliano.jpeg",
-  "Alarcon Francisco": "assets/jugadores/Alarcon-Francisco.jpeg",
-  "Zabaleta Matias": "assets/jugadores/Zabaleta-Matias.jpeg",
-  
+  "Airasca Santiago": "assets/jugadores/Airasca-Santiago.webp",
+  "Gil Gabriel": "assets/jugadores/Gil-Gabriel.webp",
+  "Gonzalez Valentin": "assets/jugadores/Gonzalez-Valentin.webp",
+  "Andino Manuel": "assets/jugadores/Andino-Manuel.webp",
+  "Ferrando Marcos": "assets/jugadores/Ferrando-Marcos.webp",
+  "Fontana Santiago": "assets/jugadores/Fontana-Santiago.webp",
+  "Monzon Maximiliano": "assets/jugadores/Monzon-Maximiliano.webp",
+  "Velázquez Gaspar": "assets/jugadores/Velazquez-Gaspar.webp",
+  "Zabaleta Javier": "assets/jugadores/Zabaleta-Javier.webp",
+  "Rios Juan": "assets/jugadores/Rios-Juan.webp",
+  "Rodriguez Joaquín": "assets/jugadores/Rodriguez-Joaquin.webp",
+  "Montiel Gonzalo": "assets/jugadores/Montiel-Gonzalo.webp",
+  "Galetto Máximo": "assets/jugadores/Galetto-Maximo.webp",
+  "Guzmán Laureano": "assets/jugadores/Guzman-Laureano.webp",
+  "Arce Timoteo": "assets/jugadores/Arce-Timoteo.webp",
+  "Bocco Martin": "assets/jugadores/Bocco-Martin.webp",
+  "Cortez Fermin": "assets/jugadores/Cortez-Fermin.webp",
+  "Ferrando Joaquín": "assets/jugadores/Ferrando-Joaquin.webp",
+  "Granero Juan Pablo": "assets/jugadores/Granero-Juan-Pablo.webp",
+  "Reguero Franco": "assets/jugadores/Reguero-Franco.webp",
+  "Rodriguez Bernabe": "assets/jugadores/Rodriguez-Bernabe.webp",
+  "Moyano Francisco": "assets/jugadores/Moyano-Francisco.webp",
+  "Victorio Juan Ignacio": "assets/jugadores/Victorio-Juan-Ignacio.webp",
+  "Zarate Ezequiel": "assets/jugadores/Zarate-Ezequiel.webp",
+  "Dopazo Martin": "assets/jugadores/Dopazo-Martin.webp",
+  "Sanchez Bruno": "assets/jugadores/Sanchez-Bruno.webp",
+  "Cataldo Ramiro": "assets/jugadores/Cataldo-Ramiro.webp",
+  "Bruera Maximiliano": "assets/jugadores/Bruera-Maximiliano.webp",
+  "Alarcon Francisco": "assets/jugadores/Alarcon-Francisco.webp",
+  "Zabaleta Matias": "assets/jugadores/Zabaleta-Matias.webp",
 };
 
 //Jugadores
@@ -104,7 +103,7 @@ function mostrarDetalle(index) {
         <div class="col-md-4 mb-4">
           <div class="card jugador-card">
             <div class="card-body">
-            <img src="${imagenesJugadores[nombre]}" alt="${nombre}" class="jugador-img mb-2">
+            <img src="${imagenesJugadores[nombre]}" alt="${nombre}" class="jugador-img mb-2" loading="lazy">
               <h5 class="card-title">${nombre}</h5>
             </div>
           </div>
