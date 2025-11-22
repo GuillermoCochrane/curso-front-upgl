d09959a9263e7f2c323758504df19de980a022ef
GuillermoCochrane <guilleac81@gmail.com>
2025-11-21 21:13:58 -0300

    Agregada func{on auxiliar que crea el cuerpo de la tarjeta del jugador

--- trabajo-final-integrador/DCI/27-11-25/pages/jugadores/scripts/jugadores.js
+++ trabajo-final-integrador/DCI/27-11-25/pages/jugadores/scripts/jugadores.js
@@ -33,3 +33,15 @@ function createElement(tagName, className = null, content = null, isHTML = false
     }
     return element;
 }
+
+function crearJugadorBody(jugador, index){
+    const $body = createElement('div', 'card-body');
+    const $title = createElement('h5', 'card-title', `${jugador.nombre}`);
+    const $text = createElement('p', 'card-text', `${jugador.posicion} | #${jugador.numero}`);
+    const $button = createElement('button', 'btn btn-outline-danger btn-detalles', `Ver Más`, false);
+    $button.setAttribute('data-bs-toggle', 'modal');
+    $button.setAttribute('data-bs-target', `#jugadorModal`);
+    $button.setAttribute('data-jugador-index', index);
+    $body.append($title, $text, $button);
+    return $body;
+}
